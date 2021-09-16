abc6f070a5a12a4a5b596bf60f10c597f582dbe1
matholv512 <matholv512@outlook.com>
2021-09-16 01:15:55 +0000

    ()

--- app/src/App.js
+++ app/src/App.js
@@ -1,36 +1,49 @@
 import './App.css';
+import React, { useState } from 'react'
+import CepConsultado from './CepConsultado.js'
 
 function App() {
-  // Função para impedir que o formulário faça reload da página
-  const submitHandLer = (e) => {
+  const [evento, setEvento] = useState([])
+
+  //função para impedir que o formulário faça reload da página
+  const submitHandler = (e) => {
     e.preventDefault();
 
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
-    console.log('Recebendo Nome:' + data.nome)
-    console.log('Recebendo Cep:' + data.cep)
+    console.log("Recebendo Nome: " + data.nome)
+    console.log("Recebendo CEP: " + data.cep)
 
-    fetch('https://3011-salmon-donkey-la6cej2q.ws-us16.gitpod.io/?cep=${data.cep}')
+    fetch(`https://3011-pink-lemur-nlrnp12d.ws-us15.gitpod.io?cep=${data.cep}`)
+    // fetch(`https://localhost:3011/?cep=14402463{data.cep}`)
     .then(response => response.json())
-    .then(console.log)
+    .then(data =>{
+      const array = convertToArray(data);
+      console.log("Array: ", array);
+      setEvento(array)
+    })
     .catch(error => console.error)
   }
 
+  const convertToArray = (obj) =>{
+    console.log("Formato Json: ", obj);
+    const arr = [obj]
+    console.log("Formato Array: ", arr);
+    return arr
+  }
+
   return (
-    <div className="conteiner">
-
-      <form onSubmit={submitHandLer}>
-
+    <div className="container">
+      <form onSubmit={submitHandler}>
         <div className="form-group" id="nome">
           <input type="text" name="nome" className="form-control" placeholder="Nome"></input>
         </div>
         <div className="form-group" id="cep">
-          <input type="text" name="cep" className="form-control" placeholder="Cep"></input>
+          <input type="text" name="cep" className="form-control" placeholder="Cep" ></input>
         </div>
-
         <button type="submit" className="btn btn-success" id="btn-consultar">Consultar CEP</button>
-
       </form>
+      <CepConsultado evento = {evento}></CepConsultado>
     </div>
   );
 }
